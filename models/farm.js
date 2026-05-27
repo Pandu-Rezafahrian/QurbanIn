d3dfeeaa2bbee89fa2a55885f0f0629f908ec578
PanduRezafahrian <rezafahrian.pandu@gmail.com>
2026-05-27 18:11:22 +0700

    feat: add Farm model with validation and associations

--- models/farm.js
+++ models/farm.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Farm extends Model {
     /**
@@ -9,17 +7,77 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    static findUserById(userId) {
+      return Farm.findAll({
+        where: {
+          userId,
+        },
+      });
+    }
+
+    get farmInfo() {
+      return `${this.name} - ${this.location}`;
+    }
+
     static associate(models) {
       // define association here
+      Farm.belongsTo(models.User, {
+        foreignKey: "userId",
+      });
+      Farm.hasMany(models.Animal, {
+        foreignKey: "farmId",
+      });
     }
   }
-  Farm.init({
-    name: DataTypes.STRING,
-    location: DataTypes.STRING,
-    description: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Farm',
-  });
+  Farm.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            args: true,
+            msg: "Nama kandang tidak boleh kosong",
+          },
+          notEmpty: {
+            args: true,
+            msg: "Nama kandang tidak boleh kosong",
+          },
+        },
+      },
+      location: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            args: true,
+            msg: "Lokasi kandang tidak boleh kosong",
+          },
+          notEmpty: {
+            args: true,
+            msg: "Lokasi kandang tidak boleh kosong",
+          },
+        },
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: "Deskripsi tidak boleh kosong",
+          },
+        },
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Farm",
+    },
+  );
   return Farm;
 };
