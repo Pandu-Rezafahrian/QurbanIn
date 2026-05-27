8a657a67d5c9b7dd7ceac1b5f73f06069577f25a
PanduRezafahrian <rezafahrian.pandu@gmail.com>
2026-05-27 17:15:03 +0700

    feat: add UserProfile model with validation and associations

--- models/userprofile.js
+++ models/userprofile.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class UserProfile extends Model {
     /**
@@ -9,17 +7,85 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    static findUserById(userId) {
+      return UserProfile.findOne({
+        where: {
+          userId,
+        },
+      });
+    }
+    get displayName() {
+      return this.fullName || "Pengguna QurbanIn";
+    }
+
     static associate(models) {
       // define association here
+      UserProfile.belongsTo(models.User, {
+        foreignKey: "userId",
+      });
     }
   }
-  UserProfile.init({
-    fullName: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    address: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'UserProfile',
-  });
+  UserProfile.init(
+    {
+      fullName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: "Nama tidak boleh kosong",
+          },
+          notNull: {
+            args: true,
+            msg: "Nama tidak boleh kosong",
+          },
+        },
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            args: true,
+            msg: "Nomor telepon tidak boleh kosong",
+          },
+          notEmpty: {
+            args: true,
+            msg: "Nomor telepon tidak boleh kosong",
+          },
+          isNumeric: {
+            args: true,
+            msg: "Nomor telepon harus berupa angka",
+          },
+          len: {
+            args: [10, 13],
+            msg: "Nomor telepon harus 10-13 digit",
+          },
+        },
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            args: true,
+            msg: "Alamat tidak boleh kosong",
+          },
+          notEmpty: {
+            args: true,
+            msg: "Alamat tidak boleh kosong",
+          },
+        },
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "UserProfile",
+    },
+  );
   return UserProfile;
 };
