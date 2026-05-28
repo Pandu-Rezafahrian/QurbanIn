e28f521458d4e0e48232b5d3e5ab6faf798ba5a4
PanduRezafahrian <rezafahrian.pandu@gmail.com>
2026-05-28 12:45:04 +0700

    feat: add OrderItem model with validation and associations + add seeders User, Farms, and Animals

--- models/orderitem.js
+++ models/orderitem.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class OrderItem extends Model {
     /**
@@ -9,15 +7,83 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    static findByOrder(orderId) {
+      return OrderItem.findAll({
+        where: {
+          orderId,
+        },
+      });
+    }
     static associate(models) {
       // define association here
+      OrderItem.belongsTo(models.Order, {
+        foreignKey: "orderId",
+      });
+      OrderItem.belongsTo(models.Animal, {
+        foreignKey: "animalId",
+      });
     }
   }
-  OrderItem.init({
-    price: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'OrderItem',
-  });
+  OrderItem.init(
+    {
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: {
+            args: true,
+            msg: "Harga tidak boleh kosong",
+          },
+          isInt: {
+            args: true,
+            msg: "Harga harus berupa angka",
+          },
+          min: {
+            args: [1500000],
+            msg: "Harga minimal Rp 1.500.000",
+          },
+        },
+      },
+      orderId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: {
+            args: true,
+            msg: "Order tidak boleh kosong",
+          },
+        },
+      },
+      animalId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: {
+            args: true,
+            msg: "Hewan tidak boleh kosong",
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "OrderItem",
+      hooks: {
+        afterCreate: async (orderItem) => {
+          const { Animal } = require("./index");
+          await Animal.update(
+            {
+              status: "terjual",
+            },
+            {
+              where: {
+                id: orderItem.animalId,
+              },
+            },
+          );
+        },
+      },
+    },
+  );
   return OrderItem;
 };
